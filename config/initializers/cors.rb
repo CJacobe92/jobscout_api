e56bb7f4661a1510cf355dcc790af58d83833f29
CJacobe92 <jacobe.christian92@gmail.com>
2023-10-10 21:15:47 +0800

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "https://jobscout-fe.vercel.app", "https://jobscout-fe.onrender.com", "https://localhost:5173"
+    origins "https://localhost:5173"
+
+    resource "*",
+      headers: :any,
+      expose: [:uid, :authorization, :refresh_token, :client, :enabled, :otp_required, :otp_enabled, :role, :tenant_id],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
+  allow do
+    origins "https://jobscout-fe.vercel.app"
 
     resource "*",
       headers: :any,
